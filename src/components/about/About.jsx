43ae0a44be0fff91ecaae24bf4f04b3c378eb70f
PriscillaAkumu <sulepriscilla@gmail.com>
2024-02-06 15:46:25 +0300

    portfolio

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,34 @@
+'use client'
+
 import { Grid, Box, Stack, Typography } from "@mui/material";
 import styles from "./about.module.css";
 import Image from "next/image";
 import Logo from "../../../public/assets/logos cos2 1.png";
+  import {styled} from 'styled-components';
 
+const StyledTypography = styled.div`
+  font-size: 36px;
+  color: #000000;
+    font-family: "Lexend";
+    font-weight: 600;
+
+  @media (max-width: 768px) {
+    font-size: 24px;
+  }
+`;
+
+const StyledParagraph = styled.div`
+  font-size: 24px;
+  font-family: Lexend;
+font-weight: 400;
+letter-spacing: 0em;
+text-align: justify;
+color:black;
+
+  @media (max-width: 768px) {
+    font-size: 18px;
+  }
+`;
 
 
 const About = () => {
@@ -10,9 +36,9 @@ const About = () => {
     <>
       <Box className={styles.flexcontainer}>
         <Stack direction="column" alignItems="center" justifyContent="center">
-          <Typography component="div" className={styles.services}>
+          <StyledTypography  >
             ABOUT US
-          </Typography>
+          </StyledTypography>
         </Stack>
 
         <Grid container  alignItems='center'  direction={{ xs: "column", sm: "column", md: "column", lg: "row" }}    sx={{  width:'100%',  }}>
@@ -20,31 +46,16 @@ const About = () => {
             <Image src={Logo} alt="logo" className={styles.images}/>
           </Grid>
           <Grid item xs={12} sm={12}  md={10} sx={{padding:{xs:0, sm:0, md:'0px 60px'}, }}>
-            <Typography component="paragraph" className={styles.text}>
+            <StyledParagraph component="paragraph" >
               At Code Optix Solutions, we are a tight-knit team of developers,
               complemented by visionary UI/UX designers. Together, we form an
               agile force, dedicated to weaving digital dreams into reality.
               With a keen eye for detail and a passion for innovation, we
               specialize in web development, e-commerce solutions, web
               applications, API development, and beyond.
-            </Typography>
+            </StyledParagraph>
           </Grid>
         </Grid>
-        {/* <Box className={styles.container} >
-            <Box className={styles.imgcontainer} padding={0}>
-            <Image src={Logo} alt="logo" className={styles.images}/>
-            </Box>
-            <Box padding={0}>
-            <Typography component="paragraph" className={styles.text} textAlign='justify'>
-              At Code Optix Solutions, we are a tight-knit team of developers,
-              complemented by visionary UI/UX designers. Together, we form an
-              agile force, dedicated to weaving digital dreams into reality.
-              With a keen eye for detail and a passion for innovation, we
-              specialize in web development, e-commerce solutions, web
-              applications, API development, and beyond.
-            </Typography>
-            </Box>
-        </Box> */}
       </Box>
     </>
   );
