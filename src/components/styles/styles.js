acb2a3d3273da7c037036619a808f5f4bc615d96
PriscillaAkumu <sulepriscilla@gmail.com>
2024-02-06 15:56:29 +0300

    portfolio

---
+++ src/components/styles/styles.js
@@ -0,0 +1,27 @@
+'use client'
+
+import styled from 'styled-components';
+
+export const StyledTypography = styled.div`
+  font-size: 36px;
+  color: #000000;
+    font-family: "Lexend";
+    font-weight: 600;
+
+  @media (max-width: 768px) {
+    font-size: 24px;
+  }
+`;
+
+export const StyledParagraph = styled.div`
+  font-size: 24px;
+  font-family: Lexend;
+font-weight: 400;
+letter-spacing: 0em;
+text-align: justify;
+color:black;
+
+  @media (max-width: 768px) {
+    font-size: 18px;
+  }
+`;
